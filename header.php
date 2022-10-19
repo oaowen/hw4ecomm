2b4af09ec1f19bb6ea12ae975a5fc3ce512e5f14
oaowen <89744805+oaowen@users.noreply.github.com>
2022-10-19 14:16:04 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <title>HW3 Owen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-    <body style="background-image: url('https://d.newsweek.com/en/full/1979380/dog-running-through-autumn-leaves.webp?w=466&h=311&f=d724190ecb5582e8a86d87dcc25351a1');">
+    <body style="background-image: url('https://cdn.vox-cdn.com/thumbor/q9Xm_9GspgADOQpPsIMfg5QWfN8=/0x0:1200x675/1200x675/filters:focal(504x242:696x434)/cdn.vox-cdn.com/uploads/chorus_image/image/67224712/pop_culter_dog_rank_alyceatinoyan_feature.0.jpg');">
  <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">University of Owen</a>
