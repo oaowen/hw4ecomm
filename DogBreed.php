33f7d37a815a3661c8ab35390a29bbf3e75cd12b
oaowen <89744805+oaowen@users.noreply.github.com>
2022-10-24 13:43:59 -0500

    Update DogBreed.php

--- DogBreed.php
+++ DogBreed.php
@@ -7,17 +7,14 @@ include('header.php');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DogBreed/title>
+    <title>INST</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Dog Breeds</h1>
+ <body>
 <table class="table table-striped">
   <thead>
     <tr>
       <th>ID</th>
-      <th>Dog Breed Name</th>
-      <th></th>
+      <th>Dog Breed</th>
     </tr>
   </thead>
   <tbody>
@@ -42,16 +39,9 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
+  <tr class="p-3 mb-2 bg-primary text-white">
     <td><?=$row["DBID"]?></td>
     <td><?=$row["DBName"]?></td>
-    
-    <td>
-      <form method="post" action="course-section.php">
-        <input type="hidden" name="id" value="<?=$row["DBID"]?>" />
-        <input type="submit" value="Sections" />
-      </form>
-    </td>
   </tr>
 <?php
   }
@@ -62,6 +52,11 @@ $conn->close();
 ?>
   </tbody>
     </table>
+    <h1>See our dog friends!</h1>
+  
+    
+    
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
