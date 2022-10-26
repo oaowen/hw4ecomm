3be02849f516a514460bd0467e00cc0c99b656e7
oaowen <89744805+oaowen@users.noreply.github.com>
2022-10-25 21:20:07 -0500

    Update DogBreed.php

--- DogBreed.php
+++ DogBreed.php
@@ -133,7 +133,7 @@ $conn->close();
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="addDogBreed">Add treats</h1>
+              <h1 class="modal-title fs-5" id="addDogBreed">Add Breed</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
